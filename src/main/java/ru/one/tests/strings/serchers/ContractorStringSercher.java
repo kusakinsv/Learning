3d27bb62f3ae605926aa2d21ef7ed7a101ffee33
Sergei <84863854+kusakinsv@users.noreply.github.com>
2021-12-24 21:31:59 +0500

    исправления

--- src/main/java/ru/one/tests/strings/serchers/ContractorStringSercher.java
+++ src/main/java/ru/one/tests/strings/serchers/ContractorStringSercher.java
@@ -1,22 +1,53 @@
-package ru.one.sabina;
+package ru.one.tests.strings.serchers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ContractorStringSercher {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(){{
+            add("ОАО Ромашка");
+            add("Ромашка ОАО");
+            add("ЗАО Ферма");
+            add("ООО \"Рога и Копыта\"");
+            add("ООО \"Автозап\"");
+            add("\"ЦПИ ООО\"");
+            add("УПРАВЛЕНИЕ РОСРЕЕСТРА ПО ПЕРМСКОМУ КРАЮ");
+            add("ИФНС РОССИИ ПО СВЕРДЛОВСКОМУ РАЙОНУ Г. ПЕРМИ");
+            add("ТЕХБУМ АО");
+            add("НПП ХАЛТУРА НПП");
+            add("ХАЛТУРА НПО АО");
+            add("\"Квартет\" ЗАО");
+            add("ООО \"ИТЦ Технополис\"");
+            add("Открытое акционерное общество \"Придумай сам\"");
+            add("Общество с ограниченной ответственностью \"Придумай сам опять\"");
+            add("Акционерное общество \"Придумай сам\"");
+            add("Запиливай Акционерное общество");
+            add("ИП Зуев Александр Васильнвич");
+            add("индивидуальный предприниматель Кыско Александр Васильнвич");
+            add("ИП Петров А. В.");
+            add("Коаорон");
+        }};
+        for (String s : list) {
+            s = findContractor(s);
+            System.out.println(s);
+        }
+    }
 
-    public String findContractor(String contractorString){
-        String result = contractorString;
+    public static String findContractor(String contractorString){
+        String result = contractorString.toLowerCase();
         if (contractorString.contains("ИП ")) {
             result = contractorString.split("ИП ")[1];
             result = result.replaceAll("\\.", "\\s");
             result = result.split(" ")[0];
-        } else if (contractorString.toLowerCase().contains("индивидуальный предприниматель")) {
-            result = contractorString.toLowerCase().split("индивидуальный предприниматель")[0];
+        } else if (contractorString.toLowerCase().contains("индивидуальный предприниматель\s")) {
+            result = contractorString.toLowerCase().split("индивидуальный предприниматель\s")[1];
             result = result.replaceAll("\\.", "\\s");
             result = result.split(" ")[0];
         } else {
-            List<String> toDelsymbols = Arrays.asList("\\s?АО|", " ОАО", " ПАО", " ООО", " ЗАО", " НПП", "АО ", "ОАО ", "ПАО ", "ООО ", "ЗАО ", "НПП ", "Общество с ограниченной ответственностью", "Акционерное общество", "\"", "\\s");
+//            List<String> toDelsymbols = Arrays.asList("[ЗО][АО]О", " ОАО", " ПАО", " ООО", " ЗАО", " НПП", "АО ", "ОАО ", "ПАО ", "ООО ", "ЗАО ", "НПП ", "Общество с ограниченной ответственностью", "Акционерное общество", "\"", "\\s");
+            List<String> toDelsymbols = Arrays.asList("\\s[ЗОП][АО]О\\b|\\b[ЗОП][АО]О\\s", "\\sАО\\b|\\bАО\\s", "\\НП[ОП]\\b|\\bНП[ОП]\\s", "Общество с ограниченной ответственностью", "\\bАкционерное общество\\s|\\sАкционерное общество\\b", "\"", "\\s");
             for (String toDelsymbol : toDelsymbols) {
             contractorString = contractorString.replaceAll(toDelsymbol, "");
         }
--- src/main/java/ru/one/tests/strings/serchers/DocumentNumberSercher.java
+++ src/main/java/ru/one/tests/strings/serchers/DocumentNumberSercher.java
@@ -1,30 +1,44 @@
-package ru.one.sabina;
+package ru.one.tests.strings.serchers;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class DocumentNumberSercher {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>() {{
+            add("Плата за выдачу наличных к чеку № '8442800' от '02/12/2021' согласно договору РКО № 3204 от '03/04/2006'");
+            add("По счету № 21-00101161069 от 13.11.2021 за перевозку грузов и транспортно-экспедиционные услуги В т.ч. НДС (20%) 139-33");
+            add("Оплата по счету № 706/3 от 22.10.2021 по Дог. № 571 от 27.10.2014 Раб.по конс. тех. сопр.сер.пр-ва РГК насосов ДЦН44С-ДТ за 3кв.2021г. Акт № 109/2021 В т.ч. НДС (20%) 2223-80");
+            add("По сч.№678от30.11.2021 г. за никель первичный марки Н1 по Спец.№07от15.09.2021г. к дог.№ 321-03/18 от 16.03.2018г.Курс Долл.США на 02.12.2021=73,9746руб. . В т.ч. НДС (20%) 5547-79");
+            add("Счет № 1093 от 14.10.2021; Оказание услуг по сбору, вывозу и утилизации эмульсии;к Договору № 080-2108/2021 от 13.10.2021 Без налога (НДС)");
+        }};
+        for (String s : list) {
+            s = findDocument(s);
+            System.out.println(s);
+        }
 
-    public String findDocument (String findString){
+    }
+
+    public static String findDocument(String findString) {
         String stringlowCase = findString.toLowerCase();
         List<String> substring = new ArrayList<>();//Arrays.asList(string.split("\\s*(\\s|,|!|\\.)\\s*"));
         List<String> schet2 = Arrays.asList("дог.", "договор", "договору", "договора");
         List<String> schet = Arrays.asList("cчет", "счету", "счета", "фактуре", "фактура", "фактуры", "сч.ф", "сч.", "с-ф", "счет-договор", "счет-договору", "счет-договора", "дог.", "договор", "договору", "договора");
         List<String> founded = new ArrayList<>();
         for (int i = 0; i < schet.size(); i++) {
-            if(stringlowCase.contains(schet.get(i))) {
+            if (stringlowCase.contains(schet.get(i))) {
                 founded.add(schet.get(i));
             }
         }
-        if (founded.size()==0){
+        if (founded.size() == 0) {
             for (int i = 0; i < schet2.size(); i++) {
-                if(stringlowCase.contains(schet2.get(i))) {
+                if (stringlowCase.contains(schet2.get(i))) {
                     founded.add(schet2.get(i));
                 }
             }
         }
-        if (founded.size()==0) return findString;
+        if (founded.size() == 0) return findString;
         String[] splitString = stringlowCase.split(founded.get(0));
         stringlowCase = splitString[1];
         substring = Arrays.asList(stringlowCase.split("\\s*(\\s|,|!|\\.)\\s*"));
@@ -35,7 +49,7 @@ public class DocumentNumberSercher {
         for (int i = 0; i < substring.size(); i++) {
             if (substring.get(i).contains("№")) preape = substring.get(i);
             if (preape.equals("№")) {
-                preape = substring.get(i+1);
+                preape = substring.get(i + 1);
                 break;
             }
             if (preape.contains("№")) preape = preape.substring(1);
