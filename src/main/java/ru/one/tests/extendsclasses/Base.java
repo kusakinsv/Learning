3d9cfa6ce44090389f9df16a139ce92e6fdb1782
SKusakin <skusakin@ibs.ru>
2022-08-11 17:26:22 +0500

    Builder

---
+++ src/main/java/ru/one/tests/extendsclasses/Base.java
@@ -0,0 +1,8 @@
+package ru.one.tests.extendsclasses;
+
+public abstract class Base {
+
+    public void start(){
+        System.out.println("Start");
+    }
+}
---
+++ src/main/java/ru/one/tests/extendsclasses/MainClassExt.java
@@ -0,0 +1,8 @@
+package ru.one.tests.extendsclasses;
+
+public class MainClassExt {
+    public static void main(String[] args) {
+        Base testclass = new One();
+        testclass.start();
+    }
+}
